12bbb2b257e512a2f347f6fefd5849457e4c9221
JoergAtGithub <JoergAtGithub@worldwartweb.com>
2022-07-03 12:07:56 +0200

    Removed unnecessary include statements Change logging to standard Qt commands Use Doxygen style comments in abletonlink.h

--- src/engine/sync/abletonlink.h
+++ src/engine/sync/abletonlink.h
@@ -1,35 +1,19 @@
 #pragma once
 
-#include <algorithm>
-#include <atomic>
-#include <chrono>
-#include <iomanip>
-#include <iostream>
-#include <thread>
-#if defined(__UNIX__)
-#include <termios.h>
-#endif
-
-#include <QObject>
-#include <QString>
-#include <QTimer>
-#include <iostream>
-
 #include <ableton/Link.hpp>
 #include <ableton/link/HostTimeFilter.hpp>
 #include "engine/channels/enginechannel.h"
 #include "engine/sync/syncable.h"
 
-/* This class manages a link session. 
- * Read & update (get & set) this session for Mixxx to be a synced Link participant (bpm & phase)
-
- * Ableton Link Readme (lib/ableton-link/README.md)
- * Documentation in the header (lib/ableton-link/include/ableton/Link.hpp)
- * Ableton provides a command line tool (LinkHut) for debugging Link programs (instructions in the Readme)
- 
- * Ableton recommends getting/setting the link session from the audio thread for maximum timing accuracy. 
- * Call the appropriate, realtime-safe functions from the audio callback to do this.
-*/
+/// This class manages a link session. 
+/// Read & update (get & set) this session for Mixxx to be a synced Link participant (bpm & phase)
+///
+/// Ableton Link Readme (lib/ableton-link/README.md)
+/// Documentation in the header (lib/ableton-link/include/ableton/Link.hpp)
+/// Ableton provides a command line tool (LinkHut) for debugging Link programs (instructions in the Readme)
+/// 
+/// Ableton recommends getting/setting the link session from the audio thread for maximum timing accuracy. 
+/// Call the appropriate, realtime-safe functions from the audio callback to do this.
 
 class AbletonLink : public QObject, public Syncable {
     Q_OBJECT
@@ -43,55 +27,55 @@ class AbletonLink : public QObject, public Syncable {
         return nullptr;
     }
 
-    // Notify a Syncable that their mode has changed. The Syncable must record
-    // this mode and return the latest mode in response to getMode().
+    /// Notify a Syncable that their mode has changed. The Syncable must record
+    /// this mode and return the latest mode in response to getMode().
     void setSyncMode(SyncMode mode) override;
 
-    // Notify a Syncable that it is now the only currently-playing syncable.
+    /// Notify a Syncable that it is now the only currently-playing syncable.
     void notifyUniquePlaying() override;
 
-    // Notify a Syncable that they should sync phase.
+    /// Notify a Syncable that they should sync phase.
     void requestSync() override;
 
-    // Must NEVER return a mode that was not set directly via
-    // notifySyncModeChanged.
+    /// Must NEVER return a mode that was not set directly via
+    /// notifySyncModeChanged.
     SyncMode getSyncMode() const override;
 
-    // Only relevant for player Syncables.
+    /// Only relevant for player Syncables.
     bool isPlaying() const override;
     bool isAudible() const override;
     bool isQuantized() const override;
 
-    // Gets the current speed of the syncable in bpm (bpm * rate slider), doesn't
-    // include scratch or FF/REW values.
+    /// Gets the current speed of the syncable in bpm (bpm * rate slider), doesn't
+    /// include scratch or FF/REW values.
     mixxx::Bpm getBpm() const override;
 
-    // Gets the beat distance as a fraction from 0 to 1
+    /// Gets the beat distance as a fraction from 0 to 1
     double getBeatDistance() const override;
 
-    // Gets the speed of the syncable if it was playing at 1.0 rate.
+    /// Gets the speed of the syncable if it was playing at 1.0 rate.
     mixxx::Bpm getBaseBpm() const override;
 
-    // The following functions are used to tell syncables about the state of the
-    // current Sync Master.
-    // Must never result in a call to
-    // SyncableListener::notifyBeatDistanceChanged or signal loops could occur.
+    /// The following functions are used to tell syncables about the state of the
+    /// current Sync Master.
+    /// Must never result in a call to
+    /// SyncableListener::notifyBeatDistanceChanged or signal loops could occur.
     void updateLeaderBeatDistance(double beatDistance) override;
 
-    // Must never result in a call to SyncableListener::notifyBpmChanged or
-    // signal loops could occur.
+    /// Must never result in a call to SyncableListener::notifyBpmChanged or
+    /// signal loops could occur.
     void updateLeaderBpm(mixxx::Bpm bpm) override;
 
     void notifyLeaderParamSource() override;
 
-    // Combines the above three calls into one, since they are often set
-    // simultaneously.  Avoids redundant recalculation that would occur by
-    // using the three calls separately.
+    /// Combines the above three calls into one, since they are often set
+    /// simultaneously.  Avoids redundant recalculation that would occur by
+    /// using the three calls separately.
     void reinitLeaderParams(double beatDistance, mixxx::Bpm baseBpm, mixxx::Bpm bpm) override;
 
-    // Must never result in a call to
-    // SyncableListener::notifyInstantaneousBpmChanged or signal loops could
-    // occur.
+    /// Must never result in a call to
+    /// SyncableListener::notifyInstantaneousBpmChanged or signal loops could
+    /// occur.
     void updateInstantaneousBpm(mixxx::Bpm bpm) override;
 
     void onCallbackStart(int sampleRate, int bufferSize);
@@ -99,7 +83,6 @@ class AbletonLink : public QObject, public Syncable {
 
   private slots:
     void testPrint() {
-        log("", "");
         nonAudioPrint();
         audioSafePrint();
     }
@@ -114,7 +97,7 @@ class AbletonLink : public QObject, public Syncable {
     std::chrono::microseconds m_currentLatency{0};
     std::chrono::microseconds m_hostTime{0};
 
-    // Uses ableton's HostTimeFilter class to create a smooth linear regression between sample time and system time
+    /// Uses ableton's HostTimeFilter class to create a smooth linear regression between sample time and system time
     void updateHostTime(size_t sampleTime) {
         m_hostTime = m_hostTimeFilter.sampleTimeToHostTime(static_cast<double>(sampleTime));
     }
@@ -134,39 +117,30 @@ class AbletonLink : public QObject, public Syncable {
     QTimer* m_pTestTimer;
     const double beat = 0.0;
 
-    template<typename T>
-    void log(const std::string& name, T val) {
-        std::cout << "link:  " << name << " - " << val << std::endl;
-    }
-
-    void logBool(const std::string& name, bool val) {
-        log(name, val ? "true" : "false");
-    }
-
     // Link getters to call from audio thread.
     void audioSafePrint() {
-        // logBool("isEnabled()", m_link.isEnabled());
-        // log("numPeers()", m_link.numPeers());
+        qDebug() << "isEnabled()" << m_link.isEnabled();
+        qDebug() << "numPeers()" << m_link.numPeers();
 
         ableton::Link::SessionState sessionState = m_link.captureAudioSessionState();
 
-        log("sessionState.tempo()", sessionState.tempo());
-        // log("sessionState.beatAtTime()", sessionState.beatAtTime(m_link.clock().micros(), getQuantum()));
-        // log("sessionState.phaseAtTime()", sessionState.phaseAtTime(m_link.clock().micros(), getQuantum()));
-        // log("sessionState.timeAtBeat(0)", sessionState.timeAtBeat(0.0, getQuantum()).count());
-        // logBool("sessionState.isPlaying()", sessionState.isPlaying());
-        // log("sessionState.timeForIsPlaying()", sessionState.timeForIsPlaying().count());
+        qDebug() << "sessionState.tempo()" << sessionState.tempo();
+        qDebug() << "sessionState.beatAtTime()" << sessionState.beatAtTime(m_link.clock().micros(), getQuantum());
+        qDebug() << "sessionState.phaseAtTime()" << sessionState.phaseAtTime(m_link.clock().micros(), getQuantum());
+        qDebug() << "sessionState.timeAtBeat(0)" << sessionState.timeAtBeat(0.0, getQuantum()).count();
+        qDebug() << "sessionState.isPlaying()" << sessionState.isPlaying();
+        qDebug() << "sessionState.timeForIsPlaying()" << sessionState.timeForIsPlaying().count();
 
         // Est. Delay (micro-seconds) between onCallbackStart() and buffer's first audio sample reaching speakers
-        // log("Est. Delay (us)", (getCurrentBufferPlayTime() - m_link.clock().micros()).count());
+        qDebug() << "Est. Delay (us)" << (getCurrentBufferPlayTime() - m_link.clock().micros()).count();
     }
 
-    // Link getters to call from non-audio thread.
+    /// Link getters to call from non-audio thread.
     void nonAudioPrint() {
-        logBool("isStartStopSyncEnabled()", m_link.isStartStopSyncEnabled());
+        qDebug() << "isStartStopSyncEnabled()" << m_link.isStartStopSyncEnabled();
     }
 
-    // Link setters to call from audio thread.
+    /// Link setters to call from audio thread.
     void audioSafeSet() {
         ableton::Link::SessionState sessionState = m_link.captureAudioSessionState();
 
@@ -181,7 +155,7 @@ class AbletonLink : public QObject, public Syncable {
         m_link.commitAudioSessionState(sessionState);
     }
 
-    // Link setters to call from non-audio thread.
+    /// Link setters to call from non-audio thread.
     void nonAudioSet() {
         m_link.enable(true);
         m_link.enableStartStopSync(true);
